aa5ed4e40de17799175ea8a8b5e9ad8db825a4e2
Richard Gu <rg779@cornell.edu>
2022-10-12 16:01:33 -0400

    Tile type impl and refector code
    - Created a Tile variant that represents all different kinds of Monopoly squares
    - Adjusted code in Monopoly.ml to reflect this new abstract representation

--- src/tile.ml
+++ src/tile.ml
@@ -8,3 +8,14 @@ type t =
   | FreeParking
   | GoToJail
   | LuxuryTax
+
+let tileName = function
+  | Property p -> Property.name p
+  | Go -> "Go"
+  | CommunityChest -> "Community Chest"
+  | IncomeTax -> "Income Tax"
+  | Chance -> "Chance"
+  | JustVisiting -> "Just Visiting"
+  | FreeParking -> "Free Parking"
+  | GoToJail -> "Go To Jail"
+  | LuxuryTax -> "Luxury Tax"
