018910c3d0e0131a9206efbfbb89aeede778f2e6
Richard Gu <rg779@cornell.edu>
2022-10-16 19:54:49 -0400

    Continuing work on player interface

--- bin/main.ml
+++ bin/main.ml
@@ -2,19 +2,19 @@ open Game
 open Monopoly
 open Random
 
-
-let rec main () = 
-  ANSITerminal.print_string [ANSITerminal.green]
-  "Welcome to Cornellopoly! In this game, you'll get to play a
-  version of the popular board game Monopoly while learning a lot about Cornell University!\n";
-  print_endline" ";
+let rec main () =
+  ANSITerminal.print_string [ ANSITerminal.green ]
+    "Welcome to Cornellopoly! In this game, you'll get to play a\n\
+    \  version of the popular board game Monopoly while learning a lot about \
+     Cornell University!\n";
+  print_endline " ";
   print_endline "Please enter your name: ";
   print_string "> ";
-  match read_line () with 
-  | name -> 
-    print_endline "Hello! ";
-    let roll = string_of_int ((Random.int 5) + 1) in 
-    let tell_roll = "Your roll is " ^ roll in 
-    print_endline tell_roll
+  match read_line () with
+  | name ->
+      print_endline "Hello! ";
+      let roll = string_of_int (Random.int 5 + 1) in
+      let tell_roll = "Your roll is " ^ roll in
+      print_endline tell_roll
 
-let () = main () 
+let () = main ()
