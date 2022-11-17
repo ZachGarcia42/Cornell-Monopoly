71489abdb052a0c238269571b55dbad2e28bfc3b
Zach Garcia <zag7@cornell.edu>
2022-11-16 19:04:37 -0500

    Refactor state  - Moved game engine functions to bin/main  - Moved player functions to player.ml

--- src/state.mli
+++ src/state.mli
@@ -8,25 +8,28 @@ val player_list : state -> Player.player list
 (** [player_list state] returns the list of players currently active in the
     game.*)
 
-val init_state : Player.player list -> state
-(** [init_state players] is the state containing [players]*)
+val purchased_properties : state -> int list
+(** [purchased_properties s] is [s.purchased_properties]*)
 
-val inform_player : state -> Player.player -> Tile.tile -> int -> unit
-(** [inform_player state player current_tile roll] prints a terminal output that
-    informs players of essential information they need to begin each turn,
-    including how much money they have and the property they are currently at. *)
+val money_jar : state -> int
+(** [money_jar s] is [s.money_jar]*)
 
-val init_players : Player.player list -> Player.player list
-(** [init_players ()] instantiates the number of desired players and adds them
-    to the players list *)
+val init_state : Player.player list -> int list -> int -> state
+(** [init_state players props money] is the state containing [players], [props],
+    and money*)
 
-val purchase_property : Player.player -> Tile.tile -> Player.player
-(** Purchases the property and updates the player's values*)
+val check_properties : state -> int -> bool
+(** [check_properties state location] returns true iff the property at index
+    [location] of the game board has been purchased. False otherwise. *)
 
-val one_turn : state -> Player.player -> Player.player * int list * int
-(** [one_turn player] represents a single turn for [player]. Returns the updated
-    player record after turn has been completed. *)
+val add_properties : int list -> int -> int list
+(** [add_properties purchased location] adds the property at index [location] to
+    the list of purchased properties. *)
 
-val take_turns : state -> state
-(** [take_turns players] represents the new player states after each player
-    takes one turn*)
+val is_property_owned : Property.t -> Player.player list -> bool
+(** [is_property_owned property] is true iff [property] is owned by one of the
+    players. *)
+
+val find_owner : Property.t -> Player.player list -> string
+(** [find_owner p players] is the name of the owner of property [p] from the
+    [players] list *)
